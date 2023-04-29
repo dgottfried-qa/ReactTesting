83611fb2a55f4dcf7d582cb8f37b7c0075753737
dgottfried-qa <dgottfried@rogers.com>
2023-04-29 18:22:34 -0400

    Test Cases 1 and 2 created

--- E2E tests/Test1.js
+++ E2E tests/Test1.js
@@ -1,50 +1,29 @@
+var assert = require('assert');
 var webdriver = require("selenium-webdriver");
 var driver = new webdriver.Builder().forBrowser('chrome').build();
-var assert = require('assert');
 
-const By = webdriver.By; // useful Locator utility to describe a query for a WebElement
-driver.navigate().to("Yahoo")
-.then(() => driver.findElement(By.css("#login-username")))
-.then(element => element.getAttribute("value"))
-.then(value => console.log(value));
 
-describe("Page loaded correctly", () =>{
-    before(() => {
-        driver.get("http://www.youtube.com");
-    });
-
-    it("should do the thing", () => {
-        console.log("\n---thing---\n");
-    });
-
-    after(() => {
-        driver.quit();
-    });
-});
-
-describe("webpage has loaded", () =>{
-    driver.navigate().to("http://localhost:3000/")
-    .then(() => driver.findElement(webdriver.By.css("img [data-testid='app-logo']")))
-    .then(() => console.log("hiiii"));
-});
-
-/*
-describe("Logo is displayed", () =>{
+ describe("webpage should load correctly", () =>{
     before(() => {
         //Load webapage
-        driver.get("http://localhost:3000/")
+        driver.get("http://localhost:3000/");
     });
-    
-    //Look for logo, re-add the await
-    const logo = driver.$('img [data-testid="app-logo"]')
-    
-    //Assert whether logo is displayed
-    assert(logo).to.do.be.null;
-    
+
+    it("should have page title", async () => {
+        const pageTitle = await driver.getTitle();
+        assert.equal(pageTitle, "React App");
+    });
+
+    it("should display link", async () => {
+        //var linkTag = driver.findElement(webdriver.By.tagName('a'));
+        var linkTag = driver.findElement(webdriver.By.css("a[data-testid='app-link']"));
+        var link = await linkTag.getAttribute("href");
+
+        assert.equal(link, "https://reactjs.org/");
+    });
+
     after(() => {
         //clean up
         driver.quit();
     });
 });
-
-*/
